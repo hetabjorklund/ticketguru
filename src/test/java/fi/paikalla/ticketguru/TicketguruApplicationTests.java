d9911fd209e332cc1bdd75fd049ae20f11c3ae04
RVeridium <riitta.icer@gmail.com>
2021-09-06 19:20:39 +0300

    initial

---
+++ src/test/java/fi/paikalla/ticketguru/TicketguruApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.paikalla.ticketguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketguruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
