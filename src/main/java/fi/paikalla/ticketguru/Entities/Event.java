60f8e5f55b93582f2fe10f70cef9e20f28f5db49
Heta Björklund <heta.bjorklund@gmail.com>
2021-09-17 16:20:38 +0300

    Päivitetty Event-entiteetti
    Odottaa vielä viime viilauksia eli sitä että joku luo Ticket-entityn johon voin viitata.

--- src/main/java/fi/paikalla/ticketguru/Entities/Event.java
+++ src/main/java/fi/paikalla/ticketguru/Entities/Event.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import java.time.*;
+import java.util.HashMap;
 
 @Entity
 @Data
@@ -15,12 +16,14 @@ public class Event extends AbstractPersistable<Long> {
 	
 	private String name;
 	private String address;
-	private Integer capacity;
+	private Integer maxCapacity;
 	private LocalDateTime startTime;
 	private LocalDateTime endTime;
 	private LocalDateTime endOfPresale;
 	private EventStatus status;
+	private String description;
 	//@OneToMany(mappedBy="event") // tämä odottaa sitä, että on luotu Ticket-olio
-	// private List<Ticket> tickets = new ArrayList<>();
+	// private List<Ticket> tickets = new ArrayList<>();	
+	private HashMap<TicketType, Double> ticketPrices; // tähän tilaisuuteen saatavilla olevat lipputyypit ja niiden hinnat
 
 }
