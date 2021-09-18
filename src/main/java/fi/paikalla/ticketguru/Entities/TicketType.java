473dba0fea1c40d1170f0ca44b5041e71b7a5b21
Jerri Mäkilä <jerri.makila@gmail.com>
2021-09-19 00:47:05 +0300

    Lisätty yksi unohtunut getter ja setter TicketType-luokasta

--- src/main/java/fi/paikalla/ticketguru/Entities/TicketType.java
+++ src/main/java/fi/paikalla/ticketguru/Entities/TicketType.java
@@ -27,6 +27,14 @@ public class TicketType {
 		this.type = type;
 	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public Event getEvent() {
 		return event;
 	}
