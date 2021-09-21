194d940ec686dc8eb5030865ad9c1e9e96a011cb
Heta Björklund <heta.bjorklund@gmail.com>
2021-09-21 11:46:35 +0300

    Injektoitu EventRepository EventControlleriin

--- src/main/java/fi/paikalla/ticketguru/Controllers/EventController.java
+++ src/main/java/fi/paikalla/ticketguru/Controllers/EventController.java
@@ -1,8 +1,13 @@
 package fi.paikalla.ticketguru.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import fi.paikalla.ticketguru.Repositories.EventRepository;
 
 @RestController
 public class EventController {
+	
+	@Autowired
+	private EventRepository eventrepo;
 
 }
