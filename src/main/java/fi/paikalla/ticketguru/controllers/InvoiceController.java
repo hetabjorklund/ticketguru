b4b32a420c87a2959e33f032ee4c20c4263a4389
Heta Björklund <heta.bjorklund@gmail.com>
2021-09-29 15:15:23 +0300

    Getit tehty
    Get all ja get one tehty

--- src/main/java/fi/paikalla/ticketguru/controllers/InvoiceController.java
+++ src/main/java/fi/paikalla/ticketguru/controllers/InvoiceController.java
@@ -1,9 +1,17 @@
 package fi.paikalla.ticketguru.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import fi.paikalla.ticketguru.Repositories.InvoiceRepository;
+import fi.paikalla.ticketguru.Entities.Invoice;
 
 @RestController
 public class InvoiceController {
@@ -11,8 +19,23 @@ public class InvoiceController {
 	@Autowired
 	private InvoiceRepository invoicerepo;
 	
-	// GET
+	@GetMapping("/invoices") // hae kaikki laskut
+	public List<Invoice> getAllInvoices() {
+		return (List<Invoice>) this.invoicerepo.findAll();		
+	}
 	
+	@GetMapping("invoices/{id}") // hae tietty lasku
+	public ResponseEntity<Optional<Invoice>> getInvoiceById(@PathVariable Long id) {
+		Optional<Invoice> invoice = this.invoicerepo.findById(id);
+		
+		if (invoice.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		else {
+			return new ResponseEntity<>(invoice, HttpStatus.OK);
+		}
+		
+	}
 	
 	
 	
