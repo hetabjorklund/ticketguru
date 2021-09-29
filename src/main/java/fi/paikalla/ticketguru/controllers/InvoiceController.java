fb4b7b407c8a220757ec391f105f4735a9b42aba
Heta Björklund <heta.bjorklund@gmail.com>
2021-09-29 14:51:28 +0300

    Aloitettu InvoiceController

---
+++ src/main/java/fi/paikalla/ticketguru/controllers/InvoiceController.java
@@ -0,0 +1,33 @@
+package fi.paikalla.ticketguru.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import fi.paikalla.ticketguru.Repositories.InvoiceRepository;
+
+@RestController
+public class InvoiceController {
+	
+	@Autowired
+	private InvoiceRepository invoicerepo;
+	
+	// GET
+	
+	
+	
+	
+	
+	
+	// POST
+	
+	
+	
+	
+	// PUT
+	
+	
+	
+	
+	// DELETE
+
+}
