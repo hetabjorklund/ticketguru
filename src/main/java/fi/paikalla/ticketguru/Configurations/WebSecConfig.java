f4070177cd10c52cb58763016d26155c784dbdc3
RVeridium <riitta.icer@gmail.com>
2021-10-11 19:33:27 +0300

    auth teoria pelaa

--- src/main/java/fi/paikalla/ticketguru/Configurations/WebSecConfig.java
+++ src/main/java/fi/paikalla/ticketguru/Configurations/WebSecConfig.java
@@ -1,43 +1,41 @@
 package fi.paikalla.ticketguru.Configurations;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled= true)
+@EnableWebSecurity
 public class WebSecConfig extends WebSecurityConfigurerAdapter {
-	@Autowired
+	//@Autowired
 	//private UserDetailServiceImp serviceImp; 
 	
 	@Override
 	protected void configure (HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-			.antMatchers("/index","/").permitAll()
 			.anyRequest().authenticated()
 			.and()
-		.formLogin()
-			//.loginPage("").permitAll()
-			.defaultSuccessUrl("/index", true)
-			.permitAll()
-			.and()
-		.logout()
-			.logoutSuccessUrl("/index")
-			.permitAll();
+			.httpBasic()
+			.realmName("TicketGuru");
 		}
 	
 	@Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(serviceImp).passwordEncoder(new BCryptPasswordEncoder());
+        //auth.userDetailsService(serviceImp).passwordEncoder(new BCryptPasswordEncoder());
+		auth
+		.inMemoryAuthentication()
+		.withUser("user").password("pssword")
+		.authorities("USER"); 
     }
 	
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-	    web
-	            .ignoring()
-	            .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
-	}
+	
 
 }
