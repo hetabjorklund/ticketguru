6e6d724794d4d2edb5b6e75566741e59876b44a6
Heta Björklund <heta.bjorklund@gmail.com>
2021-10-14 11:43:29 +0300

    Siirretty hasAvailableTickets-metodi TicketServicestä EventServiceen

--- src/main/java/fi/paikalla/ticketguru/Services/EventService.java
+++ src/main/java/fi/paikalla/ticketguru/Services/EventService.java
@@ -26,63 +26,88 @@ public class EventService {
 	@Autowired
 	private EventRepository eventrepo;
 	
+	@Autowired
+	private TicketService ticketservice;
+	
 	// eventin PATCH-toiminto
-	public Event patchEvent(JsonPatch patchDocument, Long id) {
-		
+	public Event patchEvent(JsonPatch patchDocument, Long id) {		
 		// Haetaan event eventreposta (EventControllerissa tarkistettu jo että event löytyy eikä tule virheilmoitusta)
-        Event originalEvent = eventrepo.findById(id).get();
-        
+        Event originalEvent = eventrepo.findById(id).get();        
         // Muunnetaan Event-olio JsonStructure-olioksi
         JsonStructure eventToBePatched = objectMapper.convertValue(originalEvent, JsonStructure.class);
-
         // Lisätään pyynnössä saatu JsonPatch haluttuun eventtiin
         JsonValue patchedEvent = patchDocument.apply(eventToBePatched);
-
         // Muunnetaan JsonValue-olio takaisin Event-olioksi
         Event modifiedEvent = objectMapper.convertValue(patchedEvent, Event.class);
-
         // Tallennetaan muokattu event eventrepoon
-        eventrepo.save(modifiedEvent); 
-        
+        eventrepo.save(modifiedEvent);         
         // Palautetaan muokattu event
         return modifiedEvent;
 	}
 	
-	//hakee alkpupvm perusteella
+	//hakee tapahtumat alkpupvm perusteella
 	public List<Event> getAllByStart(LocalDate date) {
 		List<Event> out = new ArrayList<Event>(); 
 		LocalDateTime timestamp = date.atStartOfDay(); //parametrin keskiyö
 		for (Event ev : eventrepo.findAll()) { //luuppaa kaikki tapahtumat
-			if (ev.getStartTime().isAfter(timestamp)) {//jos parametrin päivänä tai sen jälkeen, lisää listaan. 
+			if (ev.getStartTime().isAfter(timestamp)) {//jos parametrin päivänä tai sen jälkeen, lisää listaan 
 				out.add(ev); 
 			}
 		}
 		return out; //palauta lista. 
-	}; 
-	//hakee loppupvm perusteella
+	};
+	
+	//hakee tapahtumat loppupvm perusteella
 	public List<Event> getAllByEnd(LocalDate date) {
 		List<Event> out = new ArrayList<Event>(); 
 		LocalDateTime timestamp = date.atTime(23,59);  //muunna localdate localdatetimeksi vrk lopun perusteella
 		for (Event ev : eventrepo.findAll()) {//luuppaa kaikki tapahtumat
-			if (ev.getStartTime().isBefore(timestamp)) { //jos ennen, lisää listaan.
+			if (ev.getStartTime().isBefore(timestamp)) { //jos ennen, lisää listaan
 				out.add(ev); 
 			}
 		}
 		return out; //palauta lista
 	}; 
-	//hakee pvm välistä
+	
+	//hakee tapahtumat pvm välistä
 	public List<Event> getBtwDates(LocalDate start, LocalDate end) {
 		List<Event> out = new ArrayList<Event>(); 
 		LocalDateTime startTime = start.atStartOfDay();//muunna kuten yllä
 		LocalDateTime endTime =	end.atTime(23,59);
 		for (Event ev : eventrepo.findAll()) { //luuppaa kaikki tapahtumat
 			if (ev.getStartTime().isAfter(startTime) && ev.getStartTime().isBefore(endTime)) { 
-				//jos alkupvm jälkeen ja loppupvm ennen, lisää listaan. 
+				//jos alkupvm jälkeen ja loppupvm ennen, lisää listaan 
 				out.add(ev); 
 			};
-		};
-		
+		};		
 		return out; //palauta lista
 	}
-
+	
+	// tapahtuman jäljelläoleva lippumäärä
+	public Integer availableCapacityOfEvent(Long id) {
+		
+		Event event = eventrepo.findById(id).get();
+		int ticketsLeft = event.getMaxCapacity() - ticketservice.getTicketsByEvent(id).size();
+		return ticketsLeft;
+		
+	}
+	
+	// tarkista, onko tapahtumaan vapaita lippuja
+	public Boolean hasAvailableTickets(Long id) throws Exception {
+		
+		try {
+			Event event = eventrepo.findById(id).get();
+				
+			if (ticketservice.getTicketsByEvent(id).size() < event.getMaxCapacity()) { // jos tapahtuman lippulista on pienempi kuin maksimipaikkamäärä
+				return true; // palautetaan true eli lippuja on saatavana
+			}
+			else { // jos lippulista ei ole pienempi kuin maksimipaikkamäärä
+				return false; // palautetaan false eli lippuja ei ole saatavana
+			}
+		} catch (Exception e) { // jos eventin id:llä ei löydy tapahtumaa ja tulee virhe
+			return false; // palautetaan false
+		}			
+	}
+	
+	
 }
--- src/main/java/fi/paikalla/ticketguru/Services/TicketService.java
+++ src/main/java/fi/paikalla/ticketguru/Services/TicketService.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import fi.paikalla.ticketguru.Entities.Event;
 import fi.paikalla.ticketguru.Entities.Ticket;
 import fi.paikalla.ticketguru.Entities.TicketType;
 import fi.paikalla.ticketguru.Repositories.EventRepository;
@@ -38,25 +37,6 @@ public class TicketService {
 		}
 		return ticketlist; 		
 	}
-	
-	// tarkista, onko tapahtumaan vapaita lippuja
-	public Boolean hasAvailableTickets(Long id) throws Exception {
-		
-		try {
-			Event event = eventrepo.findById(id).get();
-			
-			if (getTicketsByEvent(id).size() < event.getMaxCapacity()) { // jos tapahtuman lippulista on pienempi kuin maksimipaikkamäärä
-				return true; // palautetaan true eli lippuja on saatavana
-			}
-			else { // jos lippulista ei ole pienempi kuin maksimipaikkamäärä
-				return false; // palautetaan false eli lippuja ei ole saatavana
-			}
-		} catch (Exception e) { // jos eventin id:llä ei löydy tapahtumaa ja tulee virhe
-			return false; // palautetaan false
-		}
-		
-	}
-
 	
 	// palauttaa lipusta Eventin id:n
 	public Long getEventIdFromTicket(Ticket ticket) {
