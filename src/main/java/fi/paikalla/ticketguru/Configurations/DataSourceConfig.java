213ff8f577181de42865430e06c0b32d55388043
Heta Björklund <heta.bjorklund@gmail.com>
2021-10-29 20:31:55 +0300

    Paikallista Dataource-kokeilua edelleen

--- src/main/java/fi/paikalla/ticketguru/Configurations/DataSourceConfig.java
+++ src/main/java/fi/paikalla/ticketguru/Configurations/DataSourceConfig.java
@@ -10,10 +10,10 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration(proxyBeanMethods = false)
-public class DataSourceConfig {
+@Configuration(proxyBeanMethods = false) // kokeile kommentoida piiloon tämä rivi ja kaikki luokan sisältö paitsi "public class DataSourceConfig"
+public class DataSourceConfig { // älä kommentoi pois
 	
-	@Bean
+	@Bean // kommentoi piiloon tästä...
     @ConfigurationProperties(prefix = "application.datasource")
     public DataSource dataSource() {
 		return DataSourceBuilder.create().build();
@@ -22,6 +22,6 @@ public class DataSourceConfig {
 	public Connection kokeilu() throws SQLException {		
 		DataSource datasource = dataSource();		
 		return datasource.getConnection();		
-	}
+	} // ... tänne
 
 }
