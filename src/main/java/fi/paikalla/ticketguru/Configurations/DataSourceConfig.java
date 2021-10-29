481ac32084c2d51314f8d1f99de6ffe74b48ef34
Heta Björklund <heta.bjorklund@gmail.com>
2021-10-29 20:10:40 +0300

    Paikallinen postgres-kokeilu DataSourceConfigilla

---
+++ src/main/java/fi/paikalla/ticketguru/Configurations/DataSourceConfig.java
@@ -0,0 +1,27 @@
+package fi.paikalla.ticketguru.Configurations;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+public class DataSourceConfig {
+	
+	@Bean
+    @ConfigurationProperties(prefix = "application.datasource")
+    public DataSource dataSource() {
+		return DataSourceBuilder.create().build();
+    }	
+	
+	public Connection kokeilu() throws SQLException {		
+		DataSource datasource = dataSource();		
+		return datasource.getConnection();		
+	}
+
+}
