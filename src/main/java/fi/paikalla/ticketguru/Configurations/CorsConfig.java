3628f19a54851f872db584f8b0798685af32ab3c
RVeridium <riitta.icer@gmail.com>
2021-11-02 18:20:14 +0200

    CORS metodit korjattu

--- src/main/java/fi/paikalla/ticketguru/Configurations/CorsConfig.java
+++ src/main/java/fi/paikalla/ticketguru/Configurations/CorsConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer{
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**");  
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");  
 		
 	}
 
