c07962f49c2ee9fb6744578e85875521e245cce2
Heta Björklund <heta.bjorklund@gmail.com>
2021-11-10 16:04:46 +0200

    EventControllerin integraatiotesti valmis
    Tämä on ennen MockBeanin kokeilua - jos se ei toimi, palaa tähän vommittiin

--- src/test/java/fi/paikalla/ticketguru/controllers/EventControllerIntegrationTest.java
+++ src/test/java/fi/paikalla/ticketguru/controllers/EventControllerIntegrationTest.java
@@ -2,6 +2,7 @@ package fi.paikalla.ticketguru.controllers;
 
 import java.time.LocalDateTime;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,27 +17,39 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import fi.paikalla.ticketguru.Entities.Event;
 import fi.paikalla.ticketguru.Repositories.EventRepository;
+import net.bytebuddy.utility.RandomString;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@WithMockUser(username = "admin", password = "password", roles = "ADMIN")
 class EventControllerIntegrationTest {
 	
 	@Autowired
     private MockMvc mvc;
 	@Autowired
 	private EventRepository eventrepositorytester;
-
+	
+	private String local = "/events"; // paikallista testaamista varten
+	private String heroku = "https://ticketguru-2021.herokuapp.com/events"; // herokun testaamista varten		
+	
+	// testaa, onko testirepository olemassa
 	@Test
-	@WithMockUser(username = "admin", password = "password", roles = "ADMIN")
-	void test() throws Exception {
+    public void testRepositoryInitializedCorrectly() {
+        Assertions.assertThat(eventrepositorytester).isNotNull();
+    }
+	
+	// testaa, löytyykö luotu testitapahtuma tietokannasta GETillä haettaessa
+	@Test
+	void testEventIsCreatedAndExists() throws Exception {
 		 
-		LocalDateTime start = LocalDateTime.of(2022,12,03,9,00);
-		LocalDateTime end = LocalDateTime.of(2022,12,03,16,00); 
-		LocalDateTime presaleend = LocalDateTime.of(2022,11,27,16,00);
+		LocalDateTime start = LocalDateTime.of(2022,12,01,00,00);
+		LocalDateTime end = LocalDateTime.of(2022,12,01,00,00); 
+		LocalDateTime presaleend = LocalDateTime.of(2022,12,01,00,00);
 		
-		String testEventName = "Testitapahtuma11";
-		Event testevent = new Event(testEventName, "Testimaailma", 10,  start, end, presaleend, "Tapahtuma testausta varten");
+		String randomEventName = RandomString.make(15);
+		
+		Event testevent = new Event(randomEventName, "Testimaailma", 10,  start, end, presaleend, "Tapahtuma testausta varten");
 		eventrepositorytester.save(testevent);
 		
 		int size = eventrepositorytester.findAll().size();
@@ -44,12 +57,39 @@ class EventControllerIntegrationTest {
 		
 		System.out.println(jsonlocation);
 					
-		mvc.perform(get("https://ticketguru-2021.herokuapp.com/events")
+		mvc.perform(get(local)
 			      .contentType(MediaType.APPLICATION_JSON))
 			      .andExpect(status().isOk())
 			      .andExpect(content()
 			      .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-			      .andExpect(jsonPath(jsonlocation).value(testEventName));	
+			      .andExpect(jsonPath(jsonlocation).value(testevent.getName()));	
+		
+	}
+	
+	// testi, jonka on tarkoitus feilata
+	@Test
+	void testShouldFail() throws Exception {
+		 
+		LocalDateTime start = LocalDateTime.of(2022,12,01,00,00);
+		LocalDateTime end = LocalDateTime.of(2022,12,01,00,00); 
+		LocalDateTime presaleend = LocalDateTime.of(2022,12,01,00,00);
+		
+		String randomEventName = RandomString.make(15);
+		
+		Event testevent = new Event(randomEventName, "Testimaailma", 10,  start, end, presaleend, "Tapahtuma testausta varten");
+		eventrepositorytester.save(testevent);
+		
+		int size = eventrepositorytester.findAll().size();
+		String jsonlocation = "$[" + (size-1) + "].name";
+		
+		System.out.println(jsonlocation);
+					
+		mvc.perform(get(local)
+			      .contentType(MediaType.APPLICATION_JSON))
+			      .andExpect(status().isOk())
+			      .andExpect(content()
+			      .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+			      .andExpect(jsonPath(jsonlocation).value("Tätä nimeä ei ole olemassa"));	
 		
 	}
 
