4c2227e7e8bfbcbf770ad2288d53d1d47d2dfa46
Heta Björklund <heta.bjorklund@gmail.com>
2021-11-10 17:00:13 +0200

    Lisätty puuttuvat importit

--- src/main/java/fi/paikalla/ticketguru/controllers/TicketController.java
+++ src/main/java/fi/paikalla/ticketguru/controllers/TicketController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import javax.json.JsonPatch;
 import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
@@ -18,6 +19,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
